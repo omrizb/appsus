937c033dbb00b8ce51bb98816cfa6d308994eeaa
einat berger <einatguberman@gmail.com>
2024-05-22 00:37:50 +0200

    Change email to mail

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,6 +1,6 @@
 export function MailPreview({ mail }) {
     return (
-        <article className={`email-preview ${mail.isRead ? 'read' : 'unread'}`}>
+        <article className={`mail-preview ${mail.isRead ? 'read' : 'unread'}`}>
             <h4>{mail.subject}</h4>
             <p>{mail.body.substring(0, 100)}...</p>
             <small>From: {mail.from} | To: {mail.to}</small>
