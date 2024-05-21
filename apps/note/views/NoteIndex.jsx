5fc5cf468ad2f5768fd341c48777968e753db98b
omribarouch <omri.barouch@safebreach.com>
2024-05-22 01:12:13 +0300

    Set basic NoteIndex, NoteList, NotePreview structure

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,4 +1,20 @@
+const { useState, useEffect } = React
+
+import { noteService } from '../services/note.service.js'
+
+import { NoteList } from '../cmps/NoteList.jsx'
 
 export function NoteIndex() {
-    return <div>note app</div>
+
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        noteService.query()
+            .then(setNotes)
+    }, [])
+
+    return <section className="note-index">
+        <h1>Notes</h1>
+        <NoteList notes={notes} />
+    </section>
 }
