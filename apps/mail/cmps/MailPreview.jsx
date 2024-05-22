bdcd22ab85a411a71365661f3b9c4a7156bf531d
einat berger <einatguberman@gmail.com>
2024-05-22 21:55:54 +0200

    Add formatDateDynamic

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,9 +1,14 @@
+import { utilService } from '../../../services/util.service.js'
+
 export function MailPreview({ mail }) {
     return (
         <article className={`mail-preview ${mail.isRead ? 'read' : 'unread'}`}>
             <h4>{mail.subject}</h4>
+
             <p>{mail.body.substring(0, 100)}...</p>
             <small>From: {mail.from} | To: {mail.to}</small>
+            <small>{utilService.formatDateDynamic(mail.sentAt)}</small>
+
         </article>
     )
 }
