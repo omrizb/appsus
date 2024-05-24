ccf67117fb7953b2c30c320263264b6ea75a48b9
einat berger <einatguberman@gmail.com>
2024-05-24 03:32:06 +0200

    Fix html and styling

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -3,12 +3,22 @@ import { utilService } from '../../../services/util.service.js'
 export function MailPreview({ mail }) {
     return (
         <article className={`mail-preview ${mail.isRead ? 'read' : 'unread'}`}>
-            <h4>{mail.subject}</h4>
-
-            <p>{mail.body.substring(0, 100)}...</p>
-            <small>From: {mail.from} | To: {mail.to}</small>
-            <small>{utilService.formatDateDynamic(mail.sentAt)}</small>
-
+            <div className="mail-header">
+                {(mail.folder === 'drafts' || mail.folder === 'inbox') && (
+                    <p>From: {mail.from}</p>
+                )}
+                {mail.folder === 'sent' && (
+                    <p>To: {mail.to}</p>
+                )}
+                {mail.folder === 'trash' && (
+                    <p>From: {mail.from} | To: {mail.to}</p>
+                )}
+                <small>{utilService.formatDateDynamic(mail.sentAt)}</small>
+            </div>
+            <div className="mail-content">
+                <p className="mail-subject">{mail.subject}</p>
+                <p className="mail-body">{mail.body.substring(0, 70)}...</p>
+            </div>
         </article>
     )
 }
