cd3c85d65bf67591ffc5a286bb4ae891799612bd
omribarouch <omri.barouch@safebreach.com>
2024-05-26 01:19:43 +0300

    Add createdAt and updatedAt to async-storage

--- services/async-storage.service.js
+++ services/async-storage.service.js
@@ -22,6 +22,8 @@ function get(entityType, entityId) {
 function post(entityType, newEntity) {
     newEntity = { ...newEntity }
     newEntity.id = _makeId()
+    newEntity.createdAt = Date.now()
+    newEntity.updatedAt = Date.now()
     return query(entityType).then(entities => {
         entities.push(newEntity)
         _save(entityType, entities)
@@ -30,6 +32,7 @@ function post(entityType, newEntity) {
 }
 
 function put(entityType, updatedEntity) {
+    updatedEntity.updatedAt = Date.now()
     return query(entityType).then(entities => {
         const idx = entities.findIndex(entity => entity.id === updatedEntity.id)
         if (idx < 0) throw new Error(`Update failed, cannot find entity with id: ${entityId} in: ${entityType}`)
