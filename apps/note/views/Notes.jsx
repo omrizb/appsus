f51af2286be2fa9831e8a289cadf05e8988afc0e
omribarouch <omri.barouch@safebreach.com>
2024-05-29 00:08:13 +0300

    Don't display pinned header if there are no pinned notes

--- apps/note/views/Notes.jsx
+++ apps/note/views/Notes.jsx
@@ -6,15 +6,16 @@ import { NoteList } from "../cmps/NoteList.jsx"
 export function Notes() {
 
     const { notes } = useOutletContext()
+    const hasPinned = notes.some(note => note.isPinned)
 
     return <div className="notes">
         <NoteAdd />
-        <div className="pinned-notes">
+        {hasPinned && <div className="pinned-notes">
             <h3>Pinned</h3>
             <NoteList notes={notes.filter(note => note.isPinned)} />
-        </div>
+        </div>}
         <div className="non-pinned-notes">
-            <h3>Others</h3>
+            {hasPinned && <h3>Others</h3>}
             <NoteList notes={notes.filter(note => !note.isPinned)} />
         </div>
     </div>
