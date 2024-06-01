437c77f6d4582fda724b230601633f85c507acd8
omribarouch <omri.barouch@safebreach.com>
2024-06-01 13:09:58 +0300

    Set media query

--- apps/note/cmps/NoteSideNav.jsx
+++ apps/note/cmps/NoteSideNav.jsx
@@ -2,10 +2,25 @@ const { NavLink } = ReactRouterDOM
 
 export function NoteSideNav() {
     return <section className="note-side-nav">
-        <NavLink to={'notes'}><span className="fa-solid i-lightbulb"></span>Notes</NavLink>
-        <NavLink to={'reminders'}><span className="fa-solid i-bell"></span>Reminders</NavLink>
-        <NavLink to={'edit-labels'}><span className="fa-solid i-pencil"></span>Edit Labels</NavLink>
-        <NavLink to={'archive'}><span className="fa-solid i-archive"></span>Archive</NavLink>
-        <NavLink to={'trash'}><span className="fa-solid i-trash"></span>Trash</NavLink>
+        <NavLink to={'notes'}>
+            <span className="fa-solid i-lightbulb"></span>
+            <span className="link">Notes</span>
+        </NavLink>
+        <NavLink to={'reminders'}>
+            <span className="fa-solid i-bell"></span>
+            <span className="link">Reminders</span>
+        </NavLink>
+        <NavLink to={'edit-labels'}>
+            <span className="fa-solid i-pencil"></span>
+            <span className="link">Edit Labels</span>
+        </NavLink>
+        <NavLink to={'archive'}>
+            <span className="fa-solid i-archive"></span>
+            <span className="link">Archive</span>
+        </NavLink>
+        <NavLink to={'trash'}>
+            <span className="fa-solid i-trash"></span>
+            <span className="link">Trash</span>
+        </NavLink>
     </section>
 }
